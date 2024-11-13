e6bb329b135924bb86fe74accb035567eba1b54a
Amal K Reji <amalreji45.amal@gmail.com>
2024-11-13 16:41:32 +0530

    design modified

--- Currency-Convertor/app.js
+++ Currency-Convertor/app.js
@@ -5,6 +5,7 @@ const dropdown  = document.querySelectorAll(".dropdown select");
 const button = document.querySelector("form button");
 const fromCurrency = document.querySelector(".from select");
 const toCurrency = document.querySelector(".to select");
+const msg = document.querySelector(".msg-container")
  
 for (let select of dropdown){
     for(currencyCode in countryList){
@@ -37,6 +38,7 @@ button.addEventListener("click",async(e) =>{
     e.preventDefault();
     let amount = document.querySelector(".amount input");
     let amountValue = amount.value;
+    
     if(amountValue === "" || amountValue < 1){
         amountValue = 1;
         amount.value = '1'
@@ -46,19 +48,26 @@ button.addEventListener("click",async(e) =>{
     
     //const URL = `${BASE_URL}/${fromCurrency.value.toLowerCase()}/${toCurrency.value.toLowerCase()}.json`;
     const URL = `${BASE_URL}/${fromCurrency.value.toLowerCase()}.json`;
+    const toCurrencyValue = toCurrency.value.toLowerCase();
+    const fromCurrencyValue = fromCurrency.value.toLowerCase();
+    console.log(fromCurrencyValue);
     
     let response = await fetch(URL)
     let data = await response.json();
-    console.log(data.usd);
-    const usdEntries = Object.entries(data.usd);
-    const toCurrencyValue = toCurrency.value.toLowerCase();
-    if (data.usd.hasOwnProperty(toCurrencyValue)) {
-        const conversionRate = data.usd[toCurrencyValue];
+    console.log(data[fromCurrencyValue]);
+    
+    
+    if (data[fromCurrencyValue].hasOwnProperty(toCurrencyValue)) {
+        const conversionRate = data[fromCurrencyValue][toCurrencyValue];
+        let totalConversionRate = amountValue * conversionRate
+        msg.innerText = `${amountValue} ${fromCurrency.value} = ${totalConversionRate} ${toCurrency.value}`
+        console.log(totalConversionRate);
+        
         console.log(`Conversion rate for ${toCurrencyValue}:`, conversionRate);
     } else {
         console.log(`Currency ${toCurrencyValue} not found.`);
     }
-    //console.log(usdEntries);
+    
     
     
 })
